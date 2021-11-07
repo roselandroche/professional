5fffb4bbbf8affe961110508ba70d9d9aa30aefb
Rose Landroche <r.landroche@gmail.com>
2021-11-07 11:42:40 -0800

    logic for accordion

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,9 +24,6 @@ module.exports = {
         short_name: `professional`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/laptop.png`, // This path is relative to the root of the site.
       },
