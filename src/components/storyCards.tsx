d739cd28ab8db1e8d7dae37fe3b8065143d30cae
Rose Landroche <r.landroche@gmail.com>
2021-11-16 18:54:21 -0800

    styling

--- src/components/storyCards.tsx
+++ src/components/storyCards.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import { Box, Heading, Text } from "theme-ui"
 
 const storyCards = props => {
@@ -10,11 +9,26 @@ const storyCards = props => {
       {stories &&
         stories.map((story, i) => {
           return (
-            <Box key={i}>
-              <Heading as="h2" children={story.category} />
-              <Heading as="h4">{story.title}</Heading>
-              <Text as="p">{story.description}</Text>
-              <Text as="p">{story.takeaway}</Text>
+            <Box
+              key={i}
+              sx={{
+                width: "1/3%",
+                border: `2px solid gray`,
+                borderRadius: "8px",
+                p: 3,
+                my: 2,
+              }}
+            >
+              <Heading as="h4" variant="styles.h4" children={story.category} />
+              <Heading as="h5" variant="styles.h5">
+                {story.title}
+              </Heading>
+              <Text as="p" variant="styles.p">
+                {story.description}
+              </Text>
+              <Text as="p" variant="styles.p">
+                Takeaway: {story.takeaway}
+              </Text>
             </Box>
           )
         })}
