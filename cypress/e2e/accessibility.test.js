a23f1f551febd50abbeddde654754c5c50b8455f
Rose Landroche <r.landroche@gmail.com>
2021-11-25 13:08:15 -0800

    accessibility test

---
+++ cypress/e2e/accessibility.test.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe("Accessibility tests", () => {
+  beforeEach(() => {
+    cy.visit("/").get("main").injectAxe()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.checkA11y()
+  })
+  it("Navigates to page 2 and checks for accessibility violations", () => {
+    cy.findByText(/jobs/i).click().checkA11y()
+  })
+})
